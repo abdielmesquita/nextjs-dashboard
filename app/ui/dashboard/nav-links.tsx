1a0c967f994ef16f16b47180902cfce030aebfce
Abdiel Mesquita <abdiel.mesquita@gmail.com>
2025-04-27 17:55:11 -0300

    feat: add functionality to edit invoices

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,6 +7,7 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import path from 'path';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,6 +23,9 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+  const [_, base, section] = pathname.split('/');
+  const currentPath = `/${base}/${section}`;
+
   return (
     <>
       {links.map((link) => {
@@ -33,7 +37,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': currentPath === link.href,
               }
             )}
           >
